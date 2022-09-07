5d14eb6646d53c5e984f99a0cebd02e629758d03
vlikcc <vli.kcc@hotmail.com>
2022-09-07 20:51:25 +0000

    07.09.2022 4. Gün

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Brands.Queries.GetListModelByDynamic;
+using Application.Features.Models.Models;
+using Application.Features.Models.Queries.GetListModel;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getListModelQuery = new GetListModelQuery { PageRequest = pageRequest };
+            ModelListModel result = await Mediator.Send(getListModelQuery);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetListModelByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListModelByDynamicQuery getListModelByDynamicQuery = new GetListModelByDynamicQuery { PageRequest = pageRequest, Dynamic = dynamic };
+            ModelListModel result = await Mediator.Send(getListModelByDynamicQuery);
+            return Ok(result);
+        }
+    }
+}
